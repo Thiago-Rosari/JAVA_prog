3f4461122a032504e0519dd794f36fb00dbf89f9
macielthiago <maciel.rosari54@gmail.com>
2025-11-30 14:42:56 -0400

    Entrada e saida padrão

---
+++ aulas/CAP_01/Entrada_Saida02.java
@@ -0,0 +1,12 @@
+package aulas.CAP_01;
+
+public class Entrada_Saida02 {
+
+    public static void main(String[] args) {
+        
+        System.out.print("Um texto!\n");
+        System.out.print("Outro texto, na linha de baixo!\n");
+        System.out.print("\tEssa linha inicia tabulada!\n");
+    }
+    
+}
---
+++ aulas/CAP_01/Variaveis.java
@@ -0,0 +1,26 @@
+package aulas.CAP_01;
+
+public class Variaveis {
+
+    public static void main(String[] args) {
+
+        /* 
+        * Cada variável precisa de um tipo e de um identificador (nome)
+        * 
+        * Os tipos que serão usados por enquanto são:
+        *   * int: números inteiros
+        *   * char: caractere
+        *   * double: número decimal/ ponto flutuante
+        */
+        
+
+        int idade;
+        char letra;
+        double altura;
+
+        int paresDeTenis = 5;
+        char letraInicial = 'D';
+        double peso = 90.5;
+    }
+    
+}
