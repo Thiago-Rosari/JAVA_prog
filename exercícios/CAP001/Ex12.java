99751e42e1e0a81a1dbdc6e43161f7b6b746b325
macielthiago <maciel.rosari54@gmail.com>
2025-12-21 17:39:01 -0400

    Create Ex12.java

---
+++ exercícios/CAP001/Ex12.java
@@ -0,0 +1,21 @@
+package exercícios.CAP001;
+
+import java.util.Scanner;
+
+public class Ex12 {
+    public static void main(String[] args) {
+        
+        float num;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Entre com um valor qualquer: ");
+
+        num = Float.parseFloat(scan.nextLine());
+
+        System.out.printf("%f\n", num);
+        System.out.printf("%.2f\n", num);
+        System.out.printf("%.3f\n", num);
+
+        scan.close();
+    }
+}
